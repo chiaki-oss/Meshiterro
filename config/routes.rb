f94d241a4d527586ac3ed8b5b59279e3d2c55f58
渡辺　千晶 <chiaki@watanabechiakinoMacBook-Air.local>
2020-05-20 17:47:59 +0900

    finish[5-5]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  # get 'users/show'  削除　下記で追加済み
   devise_for :users
   root 'post_images#index'
-  resources :post_images, only:[:new, :create, :index, :show] do
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
   	resource :favorites, only:[:create, :destroy]
 	resource :post_comments, only:[:create, :destroy]
 end
+  resources :users, only: [:show]
   #単数形/idがリクエストに含まれない
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
